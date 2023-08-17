2d1c9cbc1aa159386a9dc84e6e1d373601052d72
sidhiqali <bititude@Bititudes-Mac-mini-2.local>
2023-08-17 17:04:01 +0530

    home issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import {Route, Routes } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Main";
 import Add from "./pages/add";
 import Navbar from "./components/Navbar";
 
--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import BookCard from "../components/BookCard";
-import { useNavigate } from "react-router-dom";
+
 
 const Home = () => {
   const [books, setBooks] = useState([]);
