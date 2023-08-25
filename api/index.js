3958a58084c6a5a7b9ded51acc6db8486897d2c9
sidhiqali <sidhiqsidhiqali@gmail.com>
2023-08-25 10:00:48 +0530

    render url

--- api/index.js
+++ api/index.js
@@ -10,7 +10,9 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({ origin: "https://bookstorecrud007.netlify.app", credentials: true })
+);
 app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
