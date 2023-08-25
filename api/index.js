40a602a9ab85aa5c66c27089d8dbc876993e9704
sidhiqali <sidhiqsidhiqali@gmail.com>
2023-08-25 09:38:51 +0530

    build

--- api/index.js
+++ api/index.js
@@ -1,41 +1,39 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import authRoute from './routes/authRoute.js';
-import userRoute from './routes/userRoute.js';
-import adminRoute from './routes/adminRoute.js';
-import booksRoute from './routes/booksRoute.js';
-import db from './models/index.js';
-import cors from 'cors';
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoute from "./routes/authRoute.js";
+import userRoute from "./routes/userRoute.js";
+import booksRoute from "./routes/booksRoute.js";
+import db from "./models/index.js";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(cookieParser());
 
-app.use('/api/auth', authRoute);
-app.use('/api/user', userRoute);
-app.use('/api/admin', adminRoute);
-app.use('/api/books', booksRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/user", userRoute);
+app.use("/api/books", booksRoute);
 
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
-  const errorMessage = err.message || 'Something went wrong';
+  const errorMessage = err.message || "Something went wrong";
   res.status(errorStatus).send(errorMessage);
 });
 
 db.sequelize
   .sync()
   .then(() => {
-    console.log('Synced db.');
+    console.log("Synced db.");
   })
   .catch((err) => {
-    console.log('Failed to sync db: ' + err.message);
+    console.log("Failed to sync db: " + err.message);
   });
 
-app.get('/', (req, res) => {
-  res.send('server running successfully');
+app.get("/", (req, res) => {
+  res.send("server running successfully");
 });
 
 const port = 3000;
