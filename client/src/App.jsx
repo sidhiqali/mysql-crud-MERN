c48e1baa44cc95c421737f69d866523b9a2555b6
sidhiqali <sidhiqsidhiqali@gmail.com>
2023-08-25 09:42:38 +0530

    build

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Main";
-import Add from "./pages/add";
+import Add from "./pages/Add";
 import Navbar from "./components/Navbar";
 import Login from "./components/Login";
 import Register from "./components/Register";
